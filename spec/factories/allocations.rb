1276e1ba109b111e49dd21284348684fe3bab5ef
Rodrigo Toledo <rodrigo@rtoledo.inf.br>
2020-11-11 17:50:12 -0300

    tests fixed for allocation improve layout for allocations

--- spec/factories/allocations.rb
+++ spec/factories/allocations.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :allocation do
-    car { nil }
-    document { "MyString" }
+    car { create(:car) }
+    document { "06701375618" }
     start_at { "2020-11-11 15:34:51" }
     end_at { "2020-11-11 15:34:51" }
   end
--- spec/models/allocation_spec.rb
+++ spec/models/allocation_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Allocation, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "create and update actions" do
+    context "positive scenarios" do
+      it "easy to create" do
+        allocation = Allocation.create(attributes_for(:allocation))
+        expect(allocation).to be_truthy
+      end
+    end
+
+    context "negative scenarios" do
+      it "validate presence attributes" do
+        allocation = Allocation.new
+        expect(allocation.valid?).to be_falsey
+        expect(allocation.errors_on(:car)).to include("must exist")
+        expect(allocation.errors_on(:document)).to include("can't be blank")
+        expect(allocation.errors_on(:start_at)).to include("can't be blank")
+        expect(allocation.errors_on(:end_at)).to include("can't be blank")
+      end
+    end
+  end
 end
