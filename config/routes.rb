3d9c54594bd7d8ceea5600db20fff471f5226f52
Rodrigo Toledo <rodrigo@rtoledo.inf.br>
2020-11-11 18:14:28 -0300

    temporally allocations#index until dashboard isnt code

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   devise_for :users, only: :sessions
-  root 'home#index'
+  root 'allocations#index'
 
   resources :users, except: :new
   resources :cars, only: [:index, :show]
